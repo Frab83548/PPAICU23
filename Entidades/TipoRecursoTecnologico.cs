9e32967ad41b1ea11c33214cf56bb66274a8f0ad
83201-Pedernera-Nicolas <80838340+NicoCba@users.noreply.github.com>
2022-06-11 18:06:22 -0300

    ss

--- Entidades/TipoRecursoTecnologico.cs
+++ Entidades/TipoRecursoTecnologico.cs
@@ -8,10 +8,22 @@ namespace PPAICU23.Clases
 {
     class TipoRecursoTecnologico
     {
+        public string nombre;
+        public string descripcion;
+
+        public TipoRecursoTecnologico(string nombre, string descripcion)
+        {
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+        }
+
         public string Descripcion { get; set; }
         public string Nombre { get; set; }
 
-        
+        public string getNombre()
+        {
+            return Nombre;
+        }
 
     }
 }
---
+++ Persistencia/PersistenciaDatos.cs
@@ -0,0 +1,31 @@
+﻿using PPAICU23.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+namespace PPAICU23.Persistencia
+{
+    class PersistenciaDatos
+    {
+        public List<TipoRecursoTecnologico> tipoRecursos;
+
+        public PersistenciaDatos()
+        {
+            TipoRecursos = new List<TipoRecursoTecnologico>();
+            TipoRecursos.Add(new TipoRecursoTecnologico("balanza de precision", ""));
+            TipoRecursos.Add(new TipoRecursoTecnologico("microscopio", ""));
+            TipoRecursos.Add(new TipoRecursoTecnologico("resonador magnetico", ""));
+            TipoRecursos.Add(new TipoRecursoTecnologico("equipamiento de computo de datos de alto rendimiento", ""));
+
+            //MessageBox.Show(TipoRecursos[0].ToString());
+           
+            
+
+        }
+
+        internal List<TipoRecursoTecnologico> TipoRecursos { get => tipoRecursos; set => tipoRecursos = value; }
+    }
+}
