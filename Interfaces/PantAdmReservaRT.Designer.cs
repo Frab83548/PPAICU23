5dd04fcfca19fd972da206193192930129ea76d8
83201-Pedernera-Nicolas <80838340+NicoCba@users.noreply.github.com>
2022-06-12 19:08:40 -0300

    Carga de TR

--- Interfaces/PantAdmReservaRT.Designer.cs
+++ Interfaces/PantAdmReservaRT.Designer.cs
@@ -31,40 +31,50 @@ namespace PPAICU23.Interfaces
         {
             this.LblTiposRT = new System.Windows.Forms.Label();
             this.CBtiposRT = new System.Windows.Forms.ComboBox();
+            this.btnContinuar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LblTiposRT
             // 
             this.LblTiposRT.AutoSize = true;
             this.LblTiposRT.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblTiposRT.Location = new System.Drawing.Point(169, 42);
+            this.LblTiposRT.Location = new System.Drawing.Point(127, 34);
+            this.LblTiposRT.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LblTiposRT.Name = "LblTiposRT";
-            this.LblTiposRT.Size = new System.Drawing.Size(253, 24);
+            this.LblTiposRT.Size = new System.Drawing.Size(200, 18);
             this.LblTiposRT.TabIndex = 0;
             this.LblTiposRT.Text = "Tipos de recursos tenlogicos";
-            this.LblTiposRT.Click += new System.EventHandler(this.label1_Click);
             // 
             // CBtiposRT
             // 
             this.CBtiposRT.FormattingEnabled = true;
-            this.CBtiposRT.Items.AddRange(new object[] {
-            "Balanza de precision",
-            "Microoscopio de medicion",
-            "Lupa de calor para circuitos"});
-            this.CBtiposRT.Location = new System.Drawing.Point(153, 85);
+            this.CBtiposRT.Location = new System.Drawing.Point(115, 69);
+            this.CBtiposRT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CBtiposRT.Name = "CBtiposRT";
-            this.CBtiposRT.Size = new System.Drawing.Size(286, 24);
+            this.CBtiposRT.Size = new System.Drawing.Size(216, 21);
             this.CBtiposRT.TabIndex = 1;
-            this.CBtiposRT.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.Location = new System.Drawing.Point(251, 109);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuar.TabIndex = 2;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
             // 
             // PantAdmReservaRT
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnContinuar);
             this.Controls.Add(this.CBtiposRT);
             this.Controls.Add(this.LblTiposRT);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PantAdmReservaRT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PantAdmReservaRT";
             this.Load += new System.EventHandler(this.pant_Load);
             this.ResumeLayout(false);
@@ -76,5 +86,6 @@ namespace PPAICU23.Interfaces
 
         private System.Windows.Forms.Label LblTiposRT;
         private System.Windows.Forms.ComboBox CBtiposRT;
+        private System.Windows.Forms.Button btnContinuar;
     }
 }
